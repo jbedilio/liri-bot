372911760974bb06615659f9f6dbde69ff9fd054
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-09-24 14:21:35 -0400

    created the help dir in liri bot and finished the twitter feed

--- liri.js
+++ liri.js
@@ -1,77 +1,59 @@
-var Twitter = require('twitter');
+var ops = "";
 
-var keys = require('./keys.js');
+if(process.argv.length == 2){
+    console.log("*****Throw me a fricken bone*****\n" + 
+    'Follow this pattern to get\n' + 
+    ' my tweets: enter - node liri.js my-tweets\n' + 
+    ' my music: enter - node liri.js spotify-this-song "<title>"\n' + 
+    ' my movies: enter - node liri.js movie-this "<title>"\n' + 
+    ' random.txt: enter - node liri.js do-what-it-says');
+    return;
 
-var client = new Twitter(keys);
+}else{
 
-var params = { screen_name: 'joecitizn', count: 20};
+    for(let i = 2; i < process.argv.length; i++){
 
-client.get('statuses/user_timeline', params, (error, tweets, response) => {
-
-    if (error) {
-
-        throw error;
+        ops += process.argv[i] + "-";
     }
-    if(response.statusCode == 200){
 
-        for(var i = 0; i < tweets.length; i++){
+    console.log(ops);
 
-            process.argv[2] = tweets[i].text;
+    switch(ops){
 
-            process.argv[3] = tweets[i].created_at;
+        case 'tweets':
 
-            console.log(process.argv[2] + " " + process.argv[3]);
+        case 'my tweets':
+
+        case 'my-tweets':
+        
+        case 'my-tweets-':
+
+        var Twitter = require('twitter');           
+
+        var keys = require('./keys.js');
+
+        var client = new Twitter(keys);
+
+        var params = { screen_name: 'joecitizn', count: 20};
+
+        client.get('statuses/user_timeline', params, (error, tweets, response) => {
+
+        if (error) {
+
+            throw error;
         }
+            if(response.statusCode == 200){
 
-        //console.log(JSON.parse(tweets));
+                for(let i = 0; i < tweets.length; i++){
 
-    }
-});
+                    process.argv[2] = tweets[i].text;
 
+                    process.argv[3] = tweets[i].created_at;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var keys = require('./keys.js');
-
-//var queryUrl = "https://api.twitter.com/1.1/search/tweets.json?q=%40joecitizn&result_type=recent&count=20";
-
-var param = { screen_name: 'joecitizn' };
-
-keys.get('search/tweets', param, function (error, tweets, response) {
-
-    if (error) {
-
-        throw error;
-
-    }
-    if (response.statusCode == 200) {
-
-        //var jp = JSON.parse(tweets);
-
-        console.log('My latest tweets: ' + JSON.parse(tweets));
-
-    }
-
-});*/
+                    console.log(process.argv[2] + " " + process.argv[3]);
+                }
+            }
+        });
+        break;
+    };
+};
